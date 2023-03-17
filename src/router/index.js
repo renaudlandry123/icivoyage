1d3126267d2925d26a39b37a67091b7b8cc1dba6
renaudlandry123 <115116385+renaudlandry123@users.noreply.github.com>
2023-03-16 20:20:18 -0400

    Add files via upload

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from "vue-router";
+import AccueilView from "../views/AccueilView.vue";
+import CheckoutDetails from "../views/CheckoutDetails.vue";
+import ReservationView from "../views/ReservationView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Accueil",
+    component: AccueilView,
+  },
+  {
+    path: "/Checkout/:id",
+    name: "CheckoutDetails",
+    props: true,
+    component: CheckoutDetails,
+  },
+  {
+    path: "/Reservation",
+    name: "Reservation",
+    component: ReservationView,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
